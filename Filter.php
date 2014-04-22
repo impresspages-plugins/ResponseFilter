7bfb818a5815eeda8338ae3e9751481fd7eb4425
Mangirdas <mangirdas@impresspages.org>
2014-04-22 16:00:02 +0300

    fix for non layout responses

--- Filter.php
+++ Filter.php
@@ -9,11 +9,13 @@ namespace Plugin\ResponseFilter;
 
 
 class Filter {
-    public static function ipSendResponse(\Ip\Response\Layout $response)
+    public static function ipSendResponse($response)
     {
-        $response->setTitle('XXX title');
-        $response->setDescription('XXX description');
-        $response->setKeywords('keyword1, keyword2, keyword3');
+        if (is_object($response) && $response instanceof \Ip\Response\Layout) {
+            $response->setTitle('XXX title');
+            $response->setDescription('XXX description');
+            $response->setKeywords('keyword1, keyword2, keyword3');
+        }
         return $response;
     }
 }
